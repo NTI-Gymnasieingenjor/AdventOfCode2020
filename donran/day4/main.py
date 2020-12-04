143ea92c349e10bb19825132e1318c65509a8761
Pontus N <pontusnorr@gmail.com>
2020-12-04 11:38:02 +0100

    Day 4

---
+++ donran/day4/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import re
+data = "".join(open("in").readlines()).split("\n\n")
+reg = {
+    "byr": "(19[2-8][0-9]|199[0-9]|200[0-2])",
+    "iyr": "(201[0-9]|2020)",
+    "eyr": "(202[0-9]|2030)",
+    "hgt": "(59|6[0-9]|7[0-6])in|(1[5-8][0-9]|19[0-3])cm",
+    "hcl": "\#[0-9a-f]{6}",
+    "ecl": "amb|blu|brn|gry|grn|hzl|oth",
+    "pid": "^\d{9}$"
+}
+valid = 0
+for d in data:
+    if all([False if d.count(a) == 0 else True for a in reg]):
+        valid += 1
+
+print("Star 1: {}".format(valid))
+valid = 0
+for i in range(len(data)):
+    data[i] = data[i].replace("\n", " ").split(" ")
+    v = 0
+    for a in data[i]:
+        a = a.split(":")
+        if a[0] in reg and re.match(reg[a[0]], a[1]):
+            v += 1
+    if v == len(reg):
+        valid += 1
+print("Star 2: {}".format(valid))
