88b1730eb0e12e9fcea66a3ffe51a066271388d7
Pontus N <pontusnorr@gmail.com>
2020-12-04 12:49:35 +0100

    even more readbler

--- donran/day4/main.py
+++ donran/day4/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import re
-data = "".join(open("in").readlines()).split("\n\n")
+import re, sys
+data = list(map(lambda x: x.replace("\n", " ").split(" "), open("in").read().split("\n\n")))
 reg = {
     "byr": "(19[2-8][0-9]|199[0-9]|200[0-2])",
     "iyr": "(201[0-9]|2020)",
@@ -10,21 +10,22 @@ reg = {
     "ecl": "amb|blu|brn|gry|grn|hzl|oth",
     "pid": "^\d{9}$"
 }
-valid = 0
+valid1 = 0
+valid2 = 0
 for d in data:
-    if all([False if d.count(a) == 0 else True for a in reg]):
-        valid += 1
+    v1 = 0
+    v2 = 0
+    for field in d:
+        if field == "": continue
+        (key, value) = field.split(":")
 
-print("Star 1: {}".format(valid))
-valid = 0
-for i in range(len(data)):
-    data[i] = data[i].replace("\n", " ").split(" ")
-    v = 0
-    for a in data[i]:
-        if a == "": continue
-        (key, value) = a.split(":")
-        if key in reg and re.match(reg[key], value):
-            v += 1
-    if v == len(reg):
-        valid += 1
-print("Star 2: {}".format(valid))
+        if key in reg:
+            v1 += 1
+        else: continue
+
+        if re.match(reg[key], value):
+            v2 += 1
+    valid1 += 1 if v1 == len(reg) else 0
+    valid2 += 1 if v2 == len(reg) else 0
+print("Star 1: {}".format(valid1))
+print("Star 2: {}".format(valid2))
